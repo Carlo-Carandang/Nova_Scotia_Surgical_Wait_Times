23b263b6039afe6e020702e2f30a51004e2b2bfb
Carlo Carandang <carandangc@gmail.com>
2018-03-19 01:37:07 -0300

    edit data_analysis.rmd and related R files(2)

--- data_analysis/code/load_data.r
+++ data_analysis/code/load_data.r
@@ -1,6 +1,3 @@
 ## ---- dataset ----
 
-# library(readr)
-
-wait_times <- read.delim2(file = '../data_analysis/data/Surgical_Wait_Times_2017-09-04.csv')#,
-         #              col_names = TRUE, col_types = cols())
+wait_times <- read.delim2(file = '../data_analysis/data/Surgical_Wait_Times_2017-09-04.csv')
